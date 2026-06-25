9015dc230413f9c8d93b6742bb8fa781637b74d9
Ashutosh Awasthi <ashutoshvawasthi19@gmail.com>
2026-06-25 11:53:59 +0530

    Initial project structure

---
+++ cyber-helper-backend/src/main/java/com/ashutosh/cyberhelper/CyberHelperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ashutosh.cyberhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberHelperBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberHelperBackendApplication.class, args);
+    }
+
+}
---
+++ cyber-helper-backend/src/test/java/com/ashutosh/cyberhelper/CyberHelperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashutosh.cyberhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberHelperBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
